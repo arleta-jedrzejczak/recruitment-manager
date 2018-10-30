b0f0724918837cbe666b2d5ab5ddd09164489ae3
Arleta <laililang@gmail.com>
2018-10-30 01:02:56 +0100

    angular material and animations have been added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalendarModule } from './calendar/calendar.module';
+import { MaterialModule } from './material';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { CalendarModule } from './calendar/calendar.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CalendarModule
+    CalendarModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
